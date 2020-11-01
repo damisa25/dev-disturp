96991ea9a2f730006531ea5d83fe77f7c64a30b8
Chanchana Wicha <chanchana@wongnai.com>
2020-11-01 14:33:26 +0700

    Add submit logic

--- src/pages/scholarshipDetail/index.tsx
+++ src/pages/scholarshipDetail/index.tsx
@@ -4,74 +4,14 @@ import './index.scss';
 import '../../index.scss'
 import { useParams } from 'react-router-dom';
 import MainMenu from '../../components/menuTab/index'
-import { Row, Col, Button, Spin } from 'antd';
+import { Row, Col, Button, Spin, notification } from 'antd';
 import { CheckCircleTwoTone, MinusCircleTwoTone } from '@ant-design/icons'
-import { getScolarshipById } from '../../services/user.service'
+import { getScolarshipById, postSubmitApplication, getScholarByStudentID } from '../../services/user.service'
 import { formattedNumber } from '../../utils/numberFormat'
+import { useHistory } from "react-router-dom";
 
-const input = `
-# Getting Started with Create React App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## Available Scripts
-
-In the project directory, you can run:
-
-### \`yarn start\`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### \`yarn test\`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### \`yarn build\`
-
-Builds the app for production to the \`build\` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### \`yarn eject\`
-
-**Note: this is a one-way operation. Once you \`eject\`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can \`eject\` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except \`eject\` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use \`eject\`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-`
-
-const testChecklists = [
-  {
-    label: "นักเรียนมัธยมปลาย",
-    checked: true,
-  },
-  {
-    label: "GAPX > 2.50",
-    checked: true,
-  },
-  {
-    label: "มีหำ",
-    checked: false,
-  }
-]
+const studentId = "5f9db92b8844700abddb3c0d"
 
 interface IScholarship {
   _id: string
@@ -106,19 +46,46 @@ const ScholarshipDetail = (props: any) => {
 
   const { id }: any = useParams()
   const [scholarship, setScholarship] = useState<IScholarship>(Object)
+  const [applications, setApplications] = useState([])
   const [coverUrl, setCoverUrl] = useState('https://www.prachachat.net/wp-content/uploads/2020/01/82416410_2631246836923350_5365054154370711552_n.jpg')
   const [profileUrl, setProfileUrl] = useState('https://assets.brandinside.asia/uploads/2020/09/New-Project-2.jpg')
-  const [checklists, setChecklist] = useState(testChecklists)
+  const history = useHistory()
 
   useEffect(() => {
     console.log('use efect')
     getScolarshipById(id).then((result) => {
-      console.log(result.data)
       setScholarship(result.data)
+      getScholarByStudentID(studentId).then((result) => {
+        console.log(result.data)
+        setApplications(result.data)
+      }).catch((error) => {
+        alert(error)
+      })
     }).catch((error) => {
       alert(error)
     })
   }, [])
+
+  const submitApplication = () => {
+    postSubmitApplication(studentId, id).then((result) => {
+      if (result.status === 200) {
+        notification["success"]({
+          message: "คุณได้สมัครทุนนี้เรียบร้อยแล้ว",
+        })
+        history.push('/student/trackStatus')
+      } else {
+        notification["error"]({
+          message: "เกิดข้อผิดพลาดในการสมัครทุน",
+        })
+      }
+    }).catch((error) => {
+      notification["error"]({
+        message: error,
+      })
+    })
+  }
+
+  const isSumbmitted = applications.map((e: any) => e.scholarship._id).includes(id)
 
   const coverBackground = (
     <div className="cover-bg">
@@ -176,8 +143,8 @@ const ScholarshipDetail = (props: any) => {
                     </Col>
                     <Col span={16} style={{padding: '0px 30px 0px 15px'}}>
                       <ReactMarkdown source={scholarship.fullDescription} />
-                      <Button type="primary" className="submit-btn" onClick={() => {}}>
-                        สมัครทุน
+                      <Button disabled={isSumbmitted ? true : false} type="primary" className={isSumbmitted ? 'btn disabled' : 'btn enabled'} onClick={submitApplication}>
+                        {isSumbmitted ? "คุณได้สมัครทุนนี้แล้ว" : "สมัครทุน"}
                       </Button>
                     </Col>
                   </Row>
